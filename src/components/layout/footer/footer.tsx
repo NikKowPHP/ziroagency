3c3d7ac651de378e9cea4c25b21d788424a0fad5
NikKowPHP <nik.kow@outlook.com>
2025-01-04 15:15:45 +0100

    refactor: Adjust layout

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export function Footer() {
   return (
-    <footer className="bg-gray-50 py-20">
+    <footer className="bg-gray-50 py-12">
       <div className="container mx-auto px-6">
         <div className="flex items-center justify-between">
           <div className="">
@@ -11,7 +11,7 @@ export function Footer() {
               alt="ZIRO"
               width={150}
               height={51}
-              className="h-24 w-12"
+              className="h-30 w-30"
             />
             <div className="text-gray-500">
               Ziro sp. z o.o., ul. Mogilska 43 31-545 Kraków, Poland
