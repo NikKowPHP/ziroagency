b042a0f1fffdf15bc79da092846dac57f57424cf
NikKowPHP <nik.kow@outlook.com>
2025-01-06 10:51:36 +0100

    feat: Cache the  database data case studies

--- src/components/sections/case-studies/case-studies.tsx
+++ src/components/sections/case-studies/case-studies.tsx
@@ -1,12 +1,27 @@
-
+import { Suspense, memo } from 'react';
 import { getCaseStudies } from '@/lib/data/case-studies'
 import { CaseStudyCard } from '@/components/ui/case-study/case-study-card'
 import { type Locale } from '@/i18n'
 import { CaseStudy } from '@/domain/models/case-study.model'
+import { CaseStudiesLoader } from '@/components/sections/case-studies/case-studies-loader'
 
 interface CaseStudiesProps {
   locale: Locale
 }
+
+const CaseStudyList = memo(function CaseStudyList({ 
+  caseStudies 
+}: { 
+  caseStudies: CaseStudy[] 
+}) {
+  return (
+    <div className="container relative mx-auto lg:px-6 flex flex-col gap-16">
+      {caseStudies.map((caseStudy) => (
+        <CaseStudyCard key={caseStudy.id} caseStudy={caseStudy} />
+      ))}
+    </div>
+  );
+});
 
 export async function CaseStudies({ locale }: CaseStudiesProps) {
   const caseStudies = await getCaseStudies(locale)
@@ -14,11 +29,9 @@ export async function CaseStudies({ locale }: CaseStudiesProps) {
 
   return (
     <section id="work" className="relative overflow-hidden bg-white">
-      <div className="container relative mx-auto lg:px-6 flex flex-col gap-16">
-        {caseStudies.map((caseStudy: CaseStudy) => (
-          <CaseStudyCard key={caseStudy.id} caseStudy={caseStudy} />
-        ))}
-      </div>
+      <Suspense fallback={<CaseStudiesLoader />}>
+        <CaseStudyList caseStudies={caseStudies} />
+      </Suspense>
     </section>
   )
 }
