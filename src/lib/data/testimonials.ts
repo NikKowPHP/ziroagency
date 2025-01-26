74a98446f58f52cdc5d84bd9a1a5e24ecf04d3d7
NikKowPHP <nik.kow@outlook.com>
2025-01-26 14:21:50 +0100

    feat: Create testimonials data file

---
+++ src/lib/data/testimonials.ts
@@ -0,0 +1,63 @@
+export interface TestimonialItem {
+    id: string
+    author: string
+    role: string
+    company: string
+    quote: string
+    image: string
+    imageAlt: string
+  }
+  
+  export const testimonialItems: TestimonialItem[] = [
+    {
+      id: 'client1',
+      author: 'Client Name',
+      role: 'testimonials.client1.role',
+      company: 'testimonials.client1.company',
+      quote: 'testimonials.client1.quote',
+      image: 'https://i.postimg.cc/T1nrzdwP/BCJ9upw-V2-ARbexiidm-P5w0okz-LE.avif',
+      imageAlt: 'testimonials.client1.imageAlt'
+    },
+    {
+        id: 'client1',
+        author: 'Client Name',
+        role: 'testimonials.client1.role',
+        company: 'testimonials.client1.company',
+        quote: 'testimonials.client1.quote',
+        image: 'https://i.postimg.cc/T1nrzdwP/BCJ9upw-V2-ARbexiidm-P5w0okz-LE.avif',
+        imageAlt: 'testimonials.client1.imageAlt'
+      },
+      {
+        id: 'client1',
+        author: 'Client Name',
+        role: 'testimonials.client1.role',
+        company: 'testimonials.client1.company',
+        quote: 'testimonials.client1.quote',
+        image: 'https://i.postimg.cc/T1nrzdwP/BCJ9upw-V2-ARbexiidm-P5w0okz-LE.avif',
+        imageAlt: 'testimonials.client1.imageAlt'
+      },
+      {
+        id: 'client1',
+        author: 'Client Name',
+        role: 'testimonials.client1.role',
+        company: 'testimonials.client1.company',
+        quote: 'testimonials.client1.quote',
+        image: 'https://i.postimg.cc/T1nrzdwP/BCJ9upw-V2-ARbexiidm-P5w0okz-LE.avif',
+        imageAlt: 'testimonials.client1.imageAlt'
+      },
+      {
+        id: 'client1',
+        author: 'Client Name',
+        role: 'testimonials.client1.role',
+        company: 'testimonials.client1.company',
+        quote: 'testimonials.client1.quote',
+        image: 'https://i.postimg.cc/T1nrzdwP/BCJ9upw-V2-ARbexiidm-P5w0okz-LE.avif',
+        imageAlt: 'testimonials.client1.imageAlt'
+      },
+    // Add more testimonials
+  ]
+  
+  export async function getTestimonials(): Promise<TestimonialItem[]> {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    return testimonialItems
+  }
